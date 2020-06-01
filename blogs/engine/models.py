59627feb96998d2f48b435242014ee0ef2c95fee
Joseph Foulds <josephfoulds@googlemail.com>
2020-06-01 07:58:47 +0100

    [Schema] Create data models

--- blogs/engine/models.py
+++ blogs/engine/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    name = models.CharField(max_length=200)
+
+class Post(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
+    name = models.CharField(max_length=200)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    author = models.CharField(max_length=50)

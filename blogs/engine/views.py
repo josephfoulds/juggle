59db108bcc77fa1dcfecc5eda5edfb925a739f1a
Joseph Foulds <josephfoulds@googlemail.com>
2020-06-01 08:08:44 +0100

    [API] POST for Blog resources

--- blogs/engine/views.py
+++ blogs/engine/views.py
@@ -4,8 +4,13 @@ from django.views import View
 
 from engine.models import Blog as BlogModel
 
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
 # Index handling
 def index(request):
+	# GET / - Display all blogs
+
     # Retrieve all the blogs from the database and return them to the index template
     blogs = BlogModel.objects.all()
     context = {'blogs': blogs}
@@ -17,6 +22,32 @@ class Blog(View):
     def get(self, request, blog_id=None):
         return HttpResponse("Not yet implemented")
 
+    # POST /blog/ - Create new blog
+    def post(self, request, blog_id=None):
+        name = request.POST.get("name", "")
+
+        # Validation functions for POST data, fail with an HTTP400
+        if not name or blog_id:
+            return HttpResponse(status=400)
+
+        # Error handling for server errors
+        try:
+            # Create a new blog and save it
+            b = BlogModel(name=name)
+            b.save()
+        except Exception as e:
+            # Return HTTP500 on server error
+            return HttpResponse(status=500)
+
+        # Return HTTP200
+        return(HttpResponse())
+
+    # Exempt requests from inbuilt CSRF detection, neccessary for rapid MVP API testing
+    # nb: This should not be integrated in production unless security procedures are added to mitigate CSRF vulnerabilities
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(Blog, self).dispatch(request, *args, **kwargs)
+    
 # View for handling post resources
 class Post(View):
 

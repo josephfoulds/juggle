c73041908049a919dc00aea3da35ba9247f4a9df
Joseph Foulds <josephfoulds@googlemail.com>
2020-06-01 09:14:47 +0100

    [API] DELETE for Comment resources

--- blogs/engine/views.py
+++ blogs/engine/views.py
@@ -186,6 +186,28 @@ class Comment(View):
         # Return HTTP200
         return(HttpResponse())
 
+    # DELETE /blog/[BLOG_ID]/post/[POST_ID]/comment/[COMMENT_ID] - Delete comment
+    def delete(self, request, blog_id=None, post_id=None, comment_id=None):
+        # Validation functions for DELETE, fail with an HTTP400
+        if not blog_id or not post_id or not comment_id:
+            return HttpResponse(status=400)
+
+        # Error handling for server errors
+        try:
+            # Identify the blog from passed in blog ID
+            b = BlogModel.objects.get(id=blog_id)
+            p = PostModel.objects.get(id=post_id, blog=b)
+            c = CommentModel.objects.get(id=comment_id, post=p)
+            # Delete comment
+            c.delete()
+
+        except Exception as e:
+            # Return HTTP500 on server error
+            return HttpResponse(status=500)
+
+        # Return HTTP200
+        return(HttpResponse())
+
     # Exempt requests from inbuilt CSRF detection, neccessary for rapid MVP API testing
     # nb: This should not be integrated in production unless security procedures are added to mitigate CSRF vulnerabilities
     @method_decorator(csrf_exempt)

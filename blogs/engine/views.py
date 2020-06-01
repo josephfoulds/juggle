690972f9b662efd187de468aa9dc8bd67d8f4af5
Joseph Foulds <josephfoulds@googlemail.com>
2020-06-01 08:14:14 +0100

    [API] GET for Blog resources

--- blogs/engine/views.py
+++ blogs/engine/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render
 from django.views import View
 
 from engine.models import Blog as BlogModel
+from engine.models import Post as PostModel
 
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
 # Index handling
 def index(request):
-	# GET / - Display all blogs
+    # GET / - Display all blogs
 
     # Retrieve all the blogs from the database and return them to the index template
     blogs = BlogModel.objects.all()
@@ -19,8 +20,22 @@ def index(request):
 # View for handling blog resources
 class Blog(View):
 
+    # GET /blog/[BLOG_ID]/ - Show all posts on blog
     def get(self, request, blog_id=None):
-        return HttpResponse("Not yet implemented")
+        if not blog_id:
+            return HttpResponse(status=404)
+
+        # Identify the blog from passed in blog ID
+        try:
+            b = BlogModel.objects.get(id=blog_id)
+        except Exception as e:
+            # Return HTTP404 on missing blog
+            return HttpResponse(status=404)
+
+        p = PostModel.objects.filter(blog_id = b)
+
+        context = {'posts': p, 'blog': b}
+        return render(request, 'blog.html', context=context)
 
     # POST /blog/ - Create new blog
     def post(self, request, blog_id=None):
@@ -47,7 +62,7 @@ class Blog(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(Blog, self).dispatch(request, *args, **kwargs)
-    
+
 # View for handling post resources
 class Post(View):
 
